409ca4fe9d372ced0798dd9f7e439c6b0d430817
Serg <ochkarik1983@mail.ru@>
2020-05-09 17:00:47 +0300

    H

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from main.views import MainPageView, PagesView, AboutView, ApiPagesView
 urlpatterns = [
 	url(r'', MainPageView.as_view(), name="main"),
 	url(r'pages/', PagesView.as_view()),
-	url(r'api_pages/', ApiPagesView.as_view()),
+	url(r'^api/wp/v2/pages/', ApiPagesView.as_view()),
 	url(r'about/', AboutView.as_view()),
 ]
