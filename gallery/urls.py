b81dfddda13c57fedea16eef97ea321762fd6b02
Serg <ochkarik1983@mail.ru@>
2021-02-03 12:03:48 +0300

    J

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,5 @@ from gallery.views import *
 
 urlpatterns = [
     url(r'^$', GalleryView.as_view(), name='gallery'),
+    url(r'^$', GalleryJson.as_view()),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,14 @@
 from django.views.generic.base import TemplateView
+import json
+from django.http import HttpResponse
+from data import _array
+from django.views import View
 
 
 class GalleryView(TemplateView):
     template_name = "gallery.html"
+
+
+class GalleryJson(View):
+    def get(self, request, **kwargs):
+        return HttpResponse(json.dumps({"_array": _array}))
