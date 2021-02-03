b667401ca06242735ac88f8327ae9887ef8f55ec
Serg <ochkarik1983@mail.ru@>
2021-02-03 11:56:55 +0300

    J

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from main.views import MainPageView
+from main.views import MainPageView, SettingsView
 
 
 urlpatterns = [
 	url(r'^$', MainPageView.as_view(), name="main"),
+	url(r'^settings/$', SettingsView.as_view(), name="settings"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.views.generic.base import TemplateView
 
 class MainPageView(TemplateView):
 	template_name = "main/mainpage.html"
+
+class SettingsView(TemplateView):
+	template_name = "main/settings.html"
